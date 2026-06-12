1e5055172f87efffb745e63410ffa32d10f53634
Mythripaluri <mythrip737@gmail.com>
2026-06-12 10:20:49 +0530

    Initialize portfolio project with TanStack Start and TypeScript

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+// export default defineConfig({
+//   plugins: [
+//     tanstackStart({
+//       server: {
+//         entry: "./src/server.ts",
+//       },
+//     }),
+//     react(),
+//   ],
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    tanstackStart({
+      server: {
+        entry: "./src/server.ts",
+      },
+    }),
+    react(),
+    tailwindcss(),
+  ],
+});
